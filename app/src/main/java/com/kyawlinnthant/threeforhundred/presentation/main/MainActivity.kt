2af14c2419acecf5f690c937389514fa3cf9e89e
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2023-06-26 15:48:36 +0630

    Test commit message
    This is the description of test commit message

--- app/src/main/java/com/kyawlinnthant/threeforhundred/presentation/main/MainActivity.kt
+++ app/src/main/java/com/kyawlinnthant/threeforhundred/presentation/main/MainActivity.kt
@@ -13,6 +13,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    // test commit message best practices
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
